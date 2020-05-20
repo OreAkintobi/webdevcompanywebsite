d91922cedcca1ebdb9282b70c8590c8b18d77756
OreAkintobi <oreakintobi@gmail.com>
2020-05-20 18:06:28 +0100

    setup nodemailer + config in firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,36 @@
 const functions = require("firebase-functions");
-const config = functions.config();
 const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+const config = functions.config();
+const cors = require("cors")({ origin: true });
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "bigmansocials@gmail.com",
+    pass: "jumbojet-90",
+  },
+});
+
+let mailOptions = {
+  from: "Web Dev Solutions",
+  to: "oreakintobi@gmail.com",
+  subject: "Testing Microphone",
+  text: "SUCCESS!",
+};
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.sendMail = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    transporter.sendMail(mailOptions, (error) => {
+      if (error) {
+        response.send(error);
+      } else {
+        response.send("Message sent successfully");
+      }
+    });
+  });
+});
